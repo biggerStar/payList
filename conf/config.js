6effcb08e1fe8316c2a382471d52fcdcbf4bfcc7
songfuxing <songfuxing@baidu.com>
2017-12-22 23:36:09 +0800

    update .gitignore

--- conf/config.js
+++
@@ -1,9 +0,0 @@
-
-var host = "mongodb://@localhost/admin"
-
-function host(){
-    return host;
-}
-
-module.exports.host = host;
-
--- config.js
+++
@@ -1,9 +0,0 @@
-
-var host = "mongodb://localhost/pay"
-
-function host(){
-    return host;
-}
-
-module.exports.host = host;
-
